dca104792a5f9758f8ce3f137b87aa8c8e00ca25
Diana M Steakley-Freeman <dev@dianasteakleyfreeman.com>
2025-01-27 20:07:45 -0600

    use port 3001, minor formatting

--- api/index.js
+++ api/index.js
@@ -1,14 +1,11 @@
 const sheetDataController = require('./controllers'); // Replace with the path to your module
 const config = require('./config');
 const morgan = require('morgan');
-
 const express = require('express')
+
 const app = express()
-
 app.use(morgan('tiny'));
-
 app.get('/', sheetDataController)
-
 app.listen(config.port, () => {
-  console.log(`App listening on port ${config.port}`)
+  console.log(`API listening on port ${config.port}`)
 })
